00e94c002eea3a9634f82cc9ae01c36e3a2c8f10
unknown <samuel.wieczorek@ea.fr>
2022-03-18 06:09:46 +0100

    update mv histo plot

--- R/plot_mv.R
+++ R/plot_mv.R
@@ -1,38 +1,71 @@
-#' @param data xxx
+#' @param se An instance of the class `SummarizedExperiment`
+#' @param pattern A `character(1)` indicating the type of quantitative
+#' metadata to view. Available values are given by [qMetadata.def()]
+#' @param detailed A `logical(1)` that indicates whether to show xxx
+#' @param showValues A `logical(1)` that indicates whether to show xxx
 #' @export 
 #' @import highcharter
 #' @rdname plot-mv
-mvPerLinesHisto <- function(data){
-  stopifnot(inherits(data, "matrix"))
+mvPerLinesHisto <- function(se,
+                            pattern,
+                            detailed = FALSE,
+                            showValues = FALSE){
+  
+  if (missing(pattern) || pattern %in% c('', 'None'))
+    stop("'pattern' is missing.")
+  
+  stopifnot(inherits(se, "SummarizedExperiment"))
   coeffMax <- .1
   
-  NbNAPerCol <- colSums(is.na(data))
-  NbNAPerRow <- rowSums(is.na(data))
+  
+  data <- SummarizedExperiment::assay(se)
+  #NbNAPerCol <- colSums(is.na(data))
+  #NbNAPerRow <- rowSums(is.na(data))
+  
+  
+  mask <- match.qMetadata(qMetadata(se), 
+                          pattern = pattern, 
+                          level = typeDataset(se))
+  NbNAPerRow <- rowSums(mask)
+  
   
   nb.col <- dim(data)[2] 
   nb.na <- NbNAPerRow
   temp <- table(NbNAPerRow)
-  nb.na2barplot <- c(temp, rep(0, 1 + ncol(data) - length(temp)))
+  nb.na2barplot <- rep(0, ncol(data))
   
-  if (sum(NbNAPerRow) == 0)
-    nb.na2barplot <- rep(0, 1 + ncol(data))
+  for (i in 1:length(temp)) 
+    nb.na2barplot[as.integer(names(temp)[i])] <- temp[i]
   
-  df <- data.frame(y = nb.na2barplot[-1])
+  
+  #if (sum(NbNAPerRow) == 0)
+  #  nb.na2barplot <- rep(0, 1 + ncol(data))
+  
+  df <- data.frame(y = nb.na2barplot,
+                   y_percent = round(100 * nb.na2barplot / dim(data)[1], 
+                                     digits = 2))
+  
   myColors = rep("lightgrey", nrow(df))
-  myColors[nrow(df)] <- "red"
+  #myColors[nrow(df)] <- "red"
 
   h1 <-  highchart() %>% 
-    hc_title(text = "#[lines] with X NA values") %>% 
-    hc_add_series(data = df, type = "column", colorByPoint = TRUE) %>%
+    hc_title(text = paste0("Nb of lines with x '", pattern, "' tags")) %>% 
+    hc_add_series(data = df, 
+                  type="column", 
+                  colorByPoint = TRUE) %>%
     hc_colors(myColors) %>%
     hc_plotOptions(column = list(stacking = "normal"),
-                    animation = list(duration = 100)) %>%
+                   animation = list(duration = 100)) %>%
     hc_legend(enabled = FALSE) %>%
-    hc_xAxis(categories = row.names(df), title = list(text = "#[NA values] per line")) %>%
-    customExportMenu(fname = "mvPlot1") %>%
+    hc_xAxis(categories = row.names(df), 
+             title = list(
+               text = paste0("Nb of '", pattern, "' tags in a line"))) %>%
+    hc_yAxis(tickInterval= 1) %>%
+    customExportMenu(fname = "mvPlot") %>%
     hc_tooltip(enabled = TRUE,
-               headerFormat = '',
-               pointFormat = "{point.y} ")
+               headerFormat= '',
+               pointFormat = paste0("{point.y} lines<br>({point.y_percent}% of all lines)")
+    )
   
   return(h1)
   
