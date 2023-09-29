a0393671d0a8adc4e62a325e19b86daccfb2acf1
Samuel Wieczorek <samuel.wieczorek@cea.fr>
2023-09-29 09:51:25 +0200

    add dataset auto conversion

--- inst/extdata/examples/ex_mod_view_dataset.R
+++ inst/extdata/examples/ex_mod_view_dataset.R
@@ -15,27 +15,22 @@ ui <- fluidPage(
 
 
 server <- function(input, output, session) {
-  # data(Exp1_R25_prot, package='DaparToolshedData')
-  # obj <- convert2viz(ft)
-  # obj <- convert2viz(ft_na)
-  
+   
   #
   # Example with a QFeatures dataset
   #
-  data(Exp1_R25_pept, package='DaparToolshedData')
-  obj <- convert2viz(Exp1_R25_pept)
+  #data(Exp1_R25_pept, package='DaparToolshedData')
+  #obj <- Exp1_R25_pept
   
   #
   # Example with a series of MSnSet datasets
   #
-  # data(Exp1_R25_prot, package='DAPARdata')
-  # data(Exp1_R25_pept, package='DAPARdata')
-  # data(Exp1_R2_pept, package='DAPARdata')
-  # ll.tmp <- setNames(c(Exp1_R25_prot, Exp1_R25_pept, Exp1_R2_pept),
-  #                    nm = c('Exp1_R25_prot', 'Exp1_R25_pept', 'Exp1_R2_pept'))
-  # 
-  # obj <- convert2viz(ll.tmp)
-  
+  data(Exp1_R25_prot, package='DAPARdata')
+  data(Exp1_R25_pept, package='DAPARdata')
+  data(Exp1_R2_pept, package='DAPARdata')
+  obj <- setNames(c(Exp1_R25_prot, Exp1_R25_pept, Exp1_R2_pept),
+                     nm = c('Exp1_R25_prot', 'Exp1_R25_pept', 'Exp1_R2_pept'))
+
   
   
   mod_view_dataset_server("dataset", object = reactive({obj}))
